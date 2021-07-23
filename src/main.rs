dc1cbcd61aafd0e468dd301bdd4a2b4bb015713c
Amos Wenger <amoswenger@gmail.com>
2021-07-23 18:36:49 +0200

    terrible terrible crimes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,50 @@
-use color_eyre::Report;
-use std::future::Future;
+use color_eyre::{eyre::eyre, Report};
+use std::{future::Future, pin::Pin, time::Duration};
 
 #[tokio::main]
 async fn main() {
+    println!("Ok we're off!");
     let tup = try_join(do_stuff(), do_more_stuff()).await;
+    println!("And we're done");
     dbg!(&tup);
 }
 
 async fn do_stuff() -> Result<u64, Report> {
-    Ok(27)
-    // Err(eyre!("this don't work"))
+    tokio::time::sleep(Duration::from_millis(20)).await;
+    Err(eyre!("this don't work"))
 }
 
 async fn do_more_stuff() -> Result<String, Report> {
+    tokio::time::sleep(Duration::from_secs(10)).await;
     Ok("nice number".into())
-    // Err(eyre!("this don't work"))
 }
 
 // TODO: return as soon as either future returns Err
 async fn try_join<AR, BR, E>(
-    a: impl Future<Output = Result<AR, E>>,
-    b: impl Future<Output = Result<BR, E>>,
+    mut a: impl Future<Output = Result<AR, E>>,
+    mut b: impl Future<Output = Result<BR, E>>,
 ) -> Result<(AR, BR), E> {
-    let a = a.await?;
-    let b = b.await?;
-    Ok((a, b))
+    let mut a = unsafe { Pin::new_unchecked(&mut a) };
+    let mut b = unsafe { Pin::new_unchecked(&mut b) };
+
+    tokio::select! {
+        a = a.as_mut() => {
+            match a {
+                Ok(a) => {
+                    let b = b.await?;
+                    Ok((a, b))
+                },
+                Err(e) => Err(e),
+            }
+        },
+        b = b.as_mut() => {
+            match b {
+                Ok(b) => {
+                    let a = a.await?;
+                    Ok((a, b))
+                },
+                Err(e) => Err(e),
+            }
+        },
+    }
 }
