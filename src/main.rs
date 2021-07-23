852ab3d76895215765cc574ac6cdc7fcde2df800
Amos Wenger <amoswenger@gmail.com>
2021-07-23 18:09:32 +0200

    hey no unsafe this time

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use color_eyre::Report;
 use std::future::Future;
+use std::pin::Pin;
 
 #[tokio::main]
 async fn main() {
-    let tup = try_join(&do_stuff(), &do_more_stuff()).await;
+    let tup = try_join(Box::pin(do_stuff()), Box::pin(do_more_stuff())).await;
     dbg!(&tup);
 }
 
@@ -17,9 +18,13 @@ async fn do_more_stuff() -> Result<String, Report> {
     // Err(eyre!("this don't work"))
 }
 
+// TODO: return as soon as either future returns an error
 async fn try_join<A, B, E>(
-    f1: &dyn Future<Output = Result<A, E>>,
-    f2: &dyn Future<Output = Result<B, E>>,
+    a: Pin<Box<dyn Future<Output = Result<A, E>>>>,
+    b: Pin<Box<dyn Future<Output = Result<B, E>>>>,
 ) -> Result<(A, B), E> {
-    todo!()
+    let a = a.await?;
+    let b = b.await?;
+
+    Ok((a, b))
 }
