fe765483a85ff01e5376dd7b7c622c20720cfd71
Amos Wenger <amoswenger@gmail.com>
2021-07-23 19:22:08 +0200

    apart from the combinatorial explosion we're looking good

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@ async fn main() {
     let tup = try_join_correct(Box::pin(do_more_stuff()), Box::pin(do_stuff())).await;
     println!("And we're done");
     dbg!(&tup);
+
+    println!("Ok we're off!");
+    let tup = try_join_correct(Box::pin(do_stuff()), Box::pin(do_more_stuff())).await;
+    println!("And we're done");
+    dbg!(&tup);
 }
 
 async fn do_more_stuff() -> Result<String, Report> {
@@ -27,23 +32,38 @@ async fn do_stuff() -> Result<u64, Report> {
 fn try_join_correct<AR, BR, E>(
     a: impl Future<Output = Result<AR, E>> + Unpin,
     b: impl Future<Output = Result<BR, E>> + Unpin,
-) -> impl Future<Output = Result<(AR, BR), E>> {
-    TryFuture { a, b }
+) -> impl Future<Output = Result<(AR, BR), E>>
+where
+    AR: Unpin,
+    BR: Unpin,
+{
+    TryFuture {
+        a,
+        b,
+        a_result: None,
+        b_result: None,
+    }
 }
 
 struct TryFuture<A, B, AR, BR, E>
 where
     A: Future<Output = Result<AR, E>> + Unpin,
     B: Future<Output = Result<BR, E>> + Unpin,
+    AR: Unpin,
+    BR: Unpin,
 {
     a: A,
     b: B,
+    a_result: Option<AR>,
+    b_result: Option<BR>,
 }
 
 impl<A, B, AR, BR, E> Future for TryFuture<A, B, AR, BR, E>
 where
     A: Future<Output = Result<AR, E>> + Unpin,
     B: Future<Output = Result<BR, E>> + Unpin,
+    AR: Unpin,
+    BR: Unpin,
 {
     type Output = Result<(AR, BR), E>;
 
@@ -54,10 +74,32 @@ where
         let a = Pin::new(&mut this.a);
         let b = Pin::new(&mut this.b);
 
-        match (a.poll(cx), b.poll(cx)) {
-            (Poll::Ready(Err(e)), _) | (_, Poll::Ready(Err(e))) => Poll::Ready(Err(e)),
-            (Poll::Ready(Ok(a)), Poll::Ready(Ok(b))) => Poll::Ready(Ok((a, b))),
-            (Poll::Pending, _) | (_, Poll::Pending) => Poll::Pending,
+        if this.a_result.is_none() {
+            match a.poll(cx) {
+                Poll::Ready(a) => match a {
+                    Ok(a) => this.a_result = Some(a),
+                    Err(e) => return Poll::Ready(Err(e)),
+                },
+                Poll::Pending => {}
+            }
+        }
+
+        if this.b_result.is_none() {
+            match b.poll(cx) {
+                Poll::Ready(b) => match b {
+                    Ok(b) => this.b_result = Some(b),
+                    Err(e) => return Poll::Ready(Err(e)),
+                },
+                Poll::Pending => {}
+            }
+        }
+
+        if let (Some(_), Some(_)) = (&this.a_result, &this.b_result) {
+            // unwrap rationale: we just checked both `a_result` and `b_result` were `Some`
+            let (a, b) = (this.a_result.take().unwrap(), this.b_result.take().unwrap());
+            Poll::Ready(Ok((a, b)))
+        } else {
+            Poll::Pending
         }
     }
 }
